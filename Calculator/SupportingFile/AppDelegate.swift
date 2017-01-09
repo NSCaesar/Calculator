56b738a013fb99675a444ebf2233eddd47952146
lizhipeng <lizhipeng@tuniu.com>
2017-01-09 13:00:09 +0800

    调整文件结构

--- Calculator/SupportingFile/AppDelegate.swift
+++ Calculator/SupportingFile/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame:UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = UINavigationController(rootViewController: MainViewController())
         return true
     }
 
